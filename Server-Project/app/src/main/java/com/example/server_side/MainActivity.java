1bf9eaeb26ab9a2f9f642c4e1d67d5c5e32f6aa0
WISDOM PRAISE <wisdompraise968@gmail.com>
2019-04-26 16:21:03 +0100

    Implemented the server actvity

--- Server-Project/app/src/main/java/com/example/server_side/MainActivity.java
+++ Server-Project/app/src/main/java/com/example/server_side/MainActivity.java
@@ -1,13 +1,203 @@
 package com.example.server_side;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity  {
+    //initializes all the private properties
+    //For any server the ServerSocket and the Socket corresponding to the temp client
+    // to be activated must be initialized
+    private ServerSocket serverSocket;
+    private Socket tempClientSocket;
+
+    //here it sets the Thread initially to null
+    Thread serverThread = null;
+
+    //the SERVER_PORT is initialized which must correspond to the port of the client
+    public static final int SERVER_PORT = 5005;
+
+    //the msgList is initialized corresponding to the Linearlayout
+    private LinearLayout msgList;
+    private Handler handler;
+    private int greenColor;
+    private EditText edMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //This sets the initial content view that would be displayed
         setContentView(R.layout.activity_main);
+        setTitle("Server");
+
+        //initializes the identifier greenColor to be used anywhere within this file
+        greenColor = ContextCompat.getColor(this, R.color.green);
+
+        //initializes a new handler for message queueing
+        handler = new Handler();
+        msgList = findViewById(R.id.msgList);
+        edMessage = findViewById(R.id.edMessage);
+    }
+
+
+
+    //method to implement the different Textviews widget and display the message on
+    //the Scrollview LinearLayout...
+    public TextView textView(String message, int color) {
+        if (null == message || message.trim().isEmpty()) {
+            message = "<Empty Message>";
+        }
+        TextView tv = new TextView(this);
+        tv.setTextColor(color);
+        tv.setText(message + " [" + getTime() +"]");
+        tv.setTextSize(20);
+        tv.setPadding(0, 5, 0, 0);
+        return tv;
+    }
+
+    public void showMessage(final String message, final int color) {
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                msgList.addView(textView(message, color));
+            }
+        });
+    }
+
+
+    public void onClick(View view) {
+        if (view.getId() == R.id.start_server) {
+            msgList.removeAllViews();
+            showMessage("Server Started.", Color.BLACK);
+            this.serverThread = new Thread(new ServerThread());
+            this.serverThread.start();
+            return;
+        }
+        if (view.getId() == R.id.send_data) {
+            String msg = edMessage.getText().toString().trim();
+            showMessage("Server : " + msg, Color.BLUE);
+            sendMessage(msg);
+        }
+    }
+
+    private void sendMessage(final String message) {
+        try {
+            if (null != tempClientSocket) {
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        PrintWriter out = null;
+                        try {
+                            out = new PrintWriter(new BufferedWriter(
+                                    new OutputStreamWriter(tempClientSocket.getOutputStream())),
+                                    true);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        out.println(message);
+                    }
+                }).start();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    class ServerThread implements Runnable {
+
+        public void run() {
+            Socket socket;
+            try {
+                serverSocket = new ServerSocket(SERVER_PORT);
+                findViewById(R.id.start_server).setVisibility(View.GONE);
+            } catch (IOException e) {
+                e.printStackTrace();
+                showMessage("Error Starting Server : " + e.getMessage(), Color.RED);
+            }
+            if (null != serverSocket) {
+                while (!Thread.currentThread().isInterrupted()) {
+                    try {
+                        socket = serverSocket.accept();
+                        CommunicationThread commThread = new CommunicationThread(socket);
+                        new Thread(commThread).start();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        showMessage("Error Communicating to Client :" + e.getMessage(), Color.RED);
+                    }
+                }
+            }
+        }
+    }
+
+    class CommunicationThread implements Runnable {
+
+        private Socket clientSocket;
+
+        private BufferedReader input;
+
+        public CommunicationThread(Socket clientSocket) {
+            this.clientSocket = clientSocket;
+            tempClientSocket = clientSocket;
+            try {
+                this.input = new BufferedReader(new InputStreamReader(this.clientSocket.getInputStream()));
+            } catch (IOException e) {
+                e.printStackTrace();
+                showMessage("Error Connecting to Client!!", Color.RED);
+            }
+            showMessage("Connected to Client!!", greenColor);
+        }
+
+        public void run() {
+
+            while (!Thread.currentThread().isInterrupted()) {
+                try {
+                    String read = input.readLine();
+                    if (null == read || "Disconnect".contentEquals(read)) {
+                        Thread.interrupted();
+                        read = "Client Disconnected";
+                        showMessage("Client : " + read, greenColor);
+                        break;
+                    }
+                    showMessage("Client : " + read, greenColor);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }
+
+    }
+
+    String getTime() {
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+        return sdf.format(new Date());
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (null != serverThread) {
+            sendMessage("Disconnect");
+            serverThread.interrupt();
+            serverThread = null;
+        }
     }
 }
